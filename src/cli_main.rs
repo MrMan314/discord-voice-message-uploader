4550daef90941bda57a3beca03766cedff02a206
MrMan314 <44348373+MrMan314@users.noreply.github.com>
2024-05-22 19:26:35 -0400

    main should throw error still

--- src/cli_main.rs
+++ src/cli_main.rs
@@ -1,26 +1,23 @@
-use std::env;
+use std::{
+	env,
+	error::Error
+};
 mod messenger;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
 	let args: Vec<String> = env::args().collect();
 
 	if args.len() != 4 {
 		println!("Usage: {} token channel_id file", args[0]);
-		panic!("Invalid Usage");
+		Err("Invalid Usage")?;
 	}
 
 	let token: String = args[1].clone();
 	let chan: String = args[2].clone();
 	let file: String = args[3].clone();
 
-	match messenger::message(token, chan, file).await {
-		Ok(()) => {
-			return;
-		},
-		Err(error) => {
-			panic!("Error: {:?}", error);
-		}
-	};
+	messenger::message(token, chan, file).await?;
+	Ok(())
 }
 
