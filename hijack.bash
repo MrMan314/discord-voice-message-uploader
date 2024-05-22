022f24e1741c941ad92eb9bebf2c0377db59d772
MrMan314 <mrman314dev@gmail.com>
2024-05-22 11:28:49 -0400

    rusted!

--- hijack.bash
+++ hijack.bash
@@ -1,29 +1,27 @@
-# almost exposed token 💀
-
+set -x
 file=$1
 chan=$2
 auth=$3
 fs=`wc -c < $file`
 resp=`curl "https://canary.discord.com/api/v9/channels/$chan/attachments" \
-  -H 'accept: */*' \
-  -H "authorization: $auth" \
-  -H 'content-type: application/json' \
-  --data-raw '{"files":[{"filename":"voice-message.ogg","file_size":'$fs'}]}' \
-  --compressed \
-  --silent`
+	-H 'accept: */*' \
+	-H "authorization: $auth" \
+	-H 'content-type: application/json' \
+	--data-raw '{"files":[{"filename":"voice-message.ogg","file_size":'$fs'}]}' \
+	--compressed \
+	--silent`
 url=`jq -r ".attachments[0].upload_url" <<< $resp`
 fn=`jq -r ".attachments[0].upload_filename" <<< $resp`
 curl "$url" \
-  -X 'PUT' \
-  --data-binary "@$file" \
-  --compressed
-# copied from burpsuite and modified to strip unneeded bits (x-super-properties has iPhone so discord will allow posting)
+	-X 'PUT' \
+	--data-binary "@$file" \
+	--compressed
 curl -i -s -k -X $'POST' \
-  -H $'Host: discord.com' \
-  -H $'Content-Type: application/json' \
-  -H $'X-Debug-Options: bugReporterEnabled' \
-  -H $'Accept: */*' \
-  -H $'Authorization: '$auth -H $'Accept-Encoding: gzip, deflate' \
-  -H $'X-Super-Properties: eyJvcyI6ImlPUyIsImJyb3dzZXIiOiJEaXNjb3JkIGlPUyIsImRldmljZSI6ImlQaG9uZTksMyIsInN5c3RlbV9sb2NhbGUiOiJlbi1DQSIsImNsaWVudF92ZXJzaW9uIjoiMTcyLjAiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJicm93c2VyX3VzZXJfYWdlbnQiOiIiLCJicm93c2VyX3ZlcnNpb24iOiIiLCJvc192ZXJzaW9uIjoiMTUuNSIsImNsaWVudF9idWlsZF9udW1iZXIiOjQyNjU2LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsLCJkZXNpZ25faWQiOjB9' \
-  --data-binary $'{\"content\":\"\",\"channel_id\":\"'$chan$'\",\"type\":0,\"flags\":8192,\"attachments\":[{\"id\":\"0\",\"filename\":\"voice-message.ogg\",\"uploaded_filename\":\"'$fn$'\",\"duration_secs\":2.32,\"waveform\":\"ACETCwAFEwAAAwAyAIh9kolTjYeFUw==\"}]}' \
-  $'https://discord.com/api/v9/channels/'$chan'/messages'
+	-H $'Host: discord.com' \
+	-H $'Content-Type: application/json' \
+	-H $'Accept: */*' \
+	-H $'Authorization: '$auth \
+	-H $'Accept-Encoding: gzip, deflate' \
+	-H $'X-Super-Properties: eyJvcyI6ImlPUyIsImJyb3dzZXIiOiJEaXNjb3JkIGlPUyIsImRldmljZSI6ImlQaG9uZTksMyIsInN5c3RlbV9sb2NhbGUiOiJlbi1DQSIsImNsaWVudF92ZXJzaW9uIjoiMTcyLjAiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJicm93c2VyX3VzZXJfYWdlbnQiOiIiLCJicm93c2VyX3ZlcnNpb24iOiIiLCJvc192ZXJzaW9uIjoiMTUuNSIsImNsaWVudF9idWlsZF9udW1iZXIiOjQyNjU2LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsLCJkZXNpZ25faWQiOjB9' \
+	--data-binary $'{\"content\":\"\",\"channel_id\":\"'$chan$'\",\"type\":0,\"flags\":8192,\"attachments\":[{\"id\":\"0\",\"filename\":\"voice-message.ogg\",\"uploaded_filename\":\"'$fn$'\",\"duration_secs\":'`soxi -D $file`$',\"waveform\":\"'`dd if=/dev/urandom bs=57 count=1 | base64`'"}]}' \
+	$'https://discord.com/api/v9/channels/'$chan'/messages'
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{
 	env,
 	fs,
-	path::Path,
-    error::Error
+    error::Error,
+	path::Path
 };
 use reqwest::{
     Client,
@@ -11,6 +11,11 @@ use reqwest::{
         AUTHORIZATION,
         CONTENT_TYPE
     }
+};
+use serde_json::Value;
+use lofty::{
+	probe::Probe,
+	file::AudioFile
 };
 
 #[tokio::main]
@@ -26,11 +31,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
 	let chan: String = args[2].clone();
 	let file: String = args[3].clone();
 
-	let size = fs::metadata(file)?.len();
+	let size = fs::metadata(&file)?.len();
+	let file_buffer: Vec<u8> = fs::read(&file)?;
+	let audio_file = Probe::open(Path::new(&file))?.read()?;
 
-    let client = reqwest::Client::new();
+    let client = Client::new();
 
-    let resp = client.post(format!("https://canary.discord.com/api/v9/channels/{}/attachments", chan))
+    let resp = client.post(format!("https://discord.com/api/v9/channels/{}/attachments", chan))
         .header(ACCEPT, "*/*")
         .header(AUTHORIZATION, &token)
         .header(CONTENT_TYPE, "application/json")
@@ -39,7 +46,30 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .await?
         .text()
         .await?;
-    println!("{:#?}", resp);
+	
+	let data: Value = serde_json::from_str(&resp)?;
+	let upload_url:			&str = data["attachments"][0]["upload_url"].as_str().unwrap();
+	let upload_filename:	&str = data["attachments"][0]["upload_filename"].as_str().unwrap();
+
+	let resp = client.put(upload_url)
+		.body(file_buffer)
+		.send()
+		.await?
+		.text()
+		.await?;
+
+    let resp = client.post(format!("https://discord.com/api/v9/channels/{}/messages", chan))
+		.header(ACCEPT, "*/*")
+		.header(AUTHORIZATION, &token)
+		.header(CONTENT_TYPE, "application/json")
+		.header("X-Super-Properties", "eyJvcyI6ImlPUyIsImJyb3dzZXIiOiJEaXNjb3JkIGlPUyIsImRldmljZSI6ImlQaG9uZTksMyIsInN5c3RlbV9sb2NhbGUiOiJlbi1DQSIsImNsaWVudF92ZXJzaW9uIjoiMTcyLjAiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJicm93c2VyX3VzZXJfYWdlbnQiOiIiLCJicm93c2VyX3ZlcnNpb24iOiIiLCJvc192ZXJzaW9uIjoiMTUuNSIsImNsaWVudF9idWlsZF9udW1iZXIiOjQyNjU2LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsLCJkZXNpZ25faWQiOjB9")
+		.body(format!("{{\"content\":\"\",\"channel_id\":\"{}\",\"type\":0,\"flags\":8192,\"attachments\":[{{\"id\":\"0\",\"filename\":\"voice-message.ogg\",\"uploaded_filename\":\"{}\",\"duration_secs\":{},\"waveform\":\"////\"}}]}}", chan, upload_filename, audio_file.properties().duration().as_secs()))
+		.send()
+		.await?
+		.text()
+		.await?;
+	
+	println!("{}", resp);
 
 	Ok(())
 }
