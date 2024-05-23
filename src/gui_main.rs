072b87a84652fc8af89aee63cb8a3c92ba69ee17
MrMan314 <44348373+MrMan314@users.noreply.github.com>
2024-05-22 23:32:09 -0400

    add some ui things

--- src/gui_main.rs
+++ src/gui_main.rs
@@ -2,9 +2,13 @@ use gtk4 as gtk;
 use gtk::{
 	prelude::*,
 	glib,
+    FileDialog,
 	Application,
 	ApplicationWindow,
-	Button
+	Align::{
+		Center,
+		Start
+	}
 };
 
 #[tokio::main]
@@ -21,11 +25,40 @@ async fn main() -> glib::ExitCode {
 			.title("Discord Voice Message")
 			.build();
 
-		let button = Button::with_label("the");
-		button.connect_clicked(|_| {
-			println!("test");
-		});
-		window.set_child(Some(&button));
+		let container = gtk::Box::builder()
+			.orientation(gtk::Orientation::Vertical)
+			.margin_top(24)
+			.margin_bottom(24)
+			.margin_start(24)
+			.margin_end(24)
+			.halign(Center)
+			.valign(Center)
+			.spacing(24)
+			.build();
+
+		let title = gtk::Label::builder()
+			.label("Discord Voice Message Uploader")
+			.halign(Start)
+			.build();
+        title.add_css_class("title-2");
+        container.append(&title);
+
+        let channel_field = gtk::Entry::builder()
+            .placeholder_text("Channel ID")
+            .build();
+		container.append(&channel_field);
+
+        let token_field = gtk::Entry::builder()
+            .placeholder_text("Token")
+            .build();
+		container.append(&token_field);
+/*
+        let voice_file = gtk::FileDialog::builder()
+            .title("Choose an audio file")
+            .build();
+        container.append(&voice_file);
+*/
+		window.set_child(Some(&container));
 
 		window.present();
 	});
